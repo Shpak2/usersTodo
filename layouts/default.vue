113b17ad059a8d5306898cc7acecac4515baa958
Jack <jack@solidpower.email>
2024-11-20 08:50:52 +0100

    added tab

--- layouts/default.vue
+++ layouts/default.vue
@@ -7,24 +7,24 @@
 </template>
 
 <script setup lang="ts">
-import Header from '@/components/Header.vue';
-import SearchBar from '@/components/Main/SearchBar.vue';
+  import Header from '@/components/Header.vue';
+  import SearchBar from '@/components/Main/SearchBar.vue';
 
-import { useRoute } from 'vue-router';
-import { computed } from 'vue';
-import { useSearchStore } from '~/stores/searchStore';
+  import { useRoute } from 'vue-router';
+  import { computed } from 'vue';
+  import { useSearchStore } from '~/stores/searchStore';
 
-const route = useRoute();
-const searchStore = useSearchStore();
-const isSearchOpen = computed(() => searchStore.isSearchOpen);
+  const route = useRoute();
+  const searchStore = useSearchStore();
+  const isSearchOpen = computed(() => searchStore.isSearchOpen);
 
-const pageTitle = computed(() => {
-  const path = route.path;
-  if (path === '/') {
-    return 'Users List';
-  } else if (path.startsWith('/users/')) {
-    return `User's To-Do List`;
-  }
-});
+  const pageTitle = computed(() => {
+    const path = route.path;
+    if (path === '/') {
+      return 'Users List';
+    } else if (path.startsWith('/users/')) {
+      return `User's To-Do List`;
+    }
+  });
 </script>
 
